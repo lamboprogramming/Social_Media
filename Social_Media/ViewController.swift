b528d6e1222dc368e616e8479a0d4c5e9b9b9128
lamboprogramming <gillespielamar@gmail.com>
2022-03-22 20:14:07 -0400

    Initial Commit

---
+++ Social_Media/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social_Media
+//
+//  Created by Lamar Gillespie on 3/22/22.
+//  Copyright © 2022 Lamar Gillespie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
